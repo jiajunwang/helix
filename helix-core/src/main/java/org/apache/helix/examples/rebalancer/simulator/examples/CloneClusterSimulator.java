43855c7f6148104f04d937c0f21814fe271f64e7
Jiajun Wang <jjwang@linkedin.com>
2020-04-03 16:24:19 -0700

    Introduce rebalance simulate tool
        The tool leverages standalone ZK, Helix controller which run locally to simulate the rebalance process.
        The tool also initializes mock participant threads for simulation state transitions.
        After each operation, the tool will print the current cluster status and all state transitions happen after the previous print out.

---
+++ helix-core/src/main/java/org/apache/helix/examples/rebalancer/simulator/examples/CloneClusterSimulator.java
@@ -0,0 +1,228 @@
+package org.apache.helix.examples.rebalancer.simulator.examples;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.helix.ConfigAccessor;
+import org.apache.helix.HelixException;
+import org.apache.helix.PropertyPathBuilder;
+import org.apache.helix.examples.rebalancer.simulator.AbstractSimulator;
+import org.apache.helix.examples.rebalancer.simulator.MetadataOverwrite;
+import org.apache.helix.examples.rebalancer.simulator.operations.Operation;
+import org.apache.helix.manager.zk.ZKHelixAdmin;
+import org.apache.helix.manager.zk.ZNRecordSerializer;
+import org.apache.helix.manager.zk.client.SharedZkClientFactory;
+import org.apache.helix.model.ClusterConfig;
+import org.apache.helix.model.IdealState;
+import org.apache.helix.model.InstanceConfig;
+import org.apache.helix.model.ResourceConfig;
+import org.apache.helix.task.TaskConstants;
+import org.apache.helix.tools.commandtools.ZkCopy;
+import org.apache.helix.zookeeper.api.client.HelixZkClient;
+
+/**
+ * Clone the cluster metadata from a real running Helix cluster and then simulate based on that metadata.
+ */
+public class CloneClusterSimulator extends AbstractSimulator {
+  private static final String ZK_ADDRESS = "localhost:2199";
+  private final String _srcZkServers;
+
+  /**
+   * @param srcZkServers    the source ZkServers connection string that will be cloned
+   * @param transitionDelay
+   * @param verbose
+   */
+  public CloneClusterSimulator(String srcZkServers, int transitionDelay, boolean verbose) {
+    super(transitionDelay, verbose);
+    _srcZkServers = srcZkServers;
+  }
+
+  @Override
+  protected boolean prepareMetadata() {
+    // Clean up before clone
+    getAdmin().dropCluster(getClusterName());
+    // Clone cluster
+    echo("Cloning cluster: " + getClusterName());
+    String srcURI = String.format("zk://%s/%s", _srcZkServers, getClusterName());
+    String dstURI = String.format("zk://%s/%s", ZK_ADDRESS, getClusterName());
+    String dstPath = String.format("/%s", getClusterName());
+    try {
+      ZkCopy.main(new String[] { "--src", srcURI + "/CONFIGS", "--dst", dstURI + "/CONFIGS" });
+      ZkCopy
+          .main(new String[] { "--src", srcURI + "/CONTROLLER", "--dst", dstURI + "/CONTROLLER" });
+      ZkCopy.main(
+          new String[] { "--src", srcURI + "/IDEALSTATES", "--dst", dstURI + "/IDEALSTATES" });
+      ZkCopy.main(
+          new String[] { "--src", srcURI + "/STATEMODELDEFS", "--dst", dstURI + "/STATEMODELDEFS"
+          });
+      getZkClient().createPersistent(dstPath + "/INSTANCES");
+      getZkClient().createPersistent(dstPath + "/EXTERNALVIEW");
+      getZkClient().createPersistent(dstPath + "/LIVEINSTANCES");
+      getZkClient().createPersistent(dstPath + "/PROPERTYSTORE");
+      if (getAdmin().isInMaintenanceMode(getClusterName())) {
+        getAdmin()
+            .manuallyEnableMaintenanceMode(getClusterName(), false, "test", Collections.emptyMap());
+      }
+      String instanceConfigPath = PropertyPathBuilder.instanceConfig(getClusterName());
+      List<String> instanceConfigChildrenPath = getZkClient().getChildren(instanceConfigPath);
+      for (String instance : instanceConfigChildrenPath) {
+        getZkClient()
+            .createPersistent(PropertyPathBuilder.instanceMessage(getClusterName(), instance),
+                true);
+        getZkClient()
+            .createPersistent(PropertyPathBuilder.instanceCurrentState(getClusterName(), instance),
+                true);
+        getZkClient()
+            .createPersistent(PropertyPathBuilder.instanceError(getClusterName(), instance), true);
+        getZkClient()
+            .createPersistent(PropertyPathBuilder.instanceStatusUpdate(getClusterName(), instance),
+                true);
+        getZkClient()
+            .createPersistent(PropertyPathBuilder.instanceHistory(getClusterName(), instance),
+                true);
+      }
+    } catch (Exception e) {
+      return false;
+    }
+    return true;
+  }
+
+  private MetadataOverwrite generateOverwrites() {
+    return new MetadataOverwrite() {
+      @Override
+      public Map<String, ResourceConfig> updateResourceConfigs(
+          Map<String, ResourceConfig> resourceConfigMap, Map<String, IdealState> idealStateMap) {
+        Map<String, ResourceConfig> ret = new HashMap<>(resourceConfigMap);
+        for (Map.Entry<String, IdealState> entry : idealStateMap.entrySet()) {
+          if (entry.getValue().getStateModelDefRef().equals(TaskConstants.STATE_MODEL_NAME)
+              || !entry.getValue().getRebalanceMode().equals(IdealState.RebalanceMode.FULL_AUTO)) {
+            ret.remove(entry.getKey());
+          } else {
+            ResourceConfig resourceConfig = ret.get(entry.getKey());
+            if (resourceConfig == null) {
+              resourceConfig = new ResourceConfig(entry.getKey());
+            }
+            try {
+              resourceConfig.setPartitionCapacityMap(
+                  Collections.singletonMap(ResourceConfig.DEFAULT_PARTITION_KEY, DEFAULT_USAGE));
+            } catch (IOException e) {
+              throw new HelixException("Failed to set usage weight.", e);
+            }
+            resourceConfig.setPreferenceLists(Collections.EMPTY_MAP);
+          }
+        }
+        return ret;
+      }
+
+      @Override
+      public Map<String, IdealState> updateIdealStates(Map<String, IdealState> idealStateMap) {
+        Map<String, IdealState> ret = new HashMap<>(idealStateMap);
+        for (Map.Entry<String, IdealState> entry : idealStateMap.entrySet()) {
+          if (entry.getValue().getStateModelDefRef().equals("Task") || !entry.getValue()
+              .getRebalanceMode().equals(IdealState.RebalanceMode.FULL_AUTO)) {
+            ret.remove(entry.getKey());
+          }
+        }
+        return ret;
+      }
+
+      @Override
+      public Map<String, InstanceConfig> updateInstanceConfig(ClusterConfig clusterConfig,
+          Map<String, InstanceConfig> instanceConfigMap) {
+        for (InstanceConfig instanceConfig : instanceConfigMap.values()) {
+          instanceConfig.getRecord()
+              .setMapField(InstanceConfig.InstanceConfigProperty.HELIX_DISABLED_PARTITION.name(),
+                  Collections.emptyMap());
+          // TODO: refine the fault zone fetch logic here.
+          getNodeToZoneMap().put(instanceConfig.getInstanceName(),
+              instanceConfig.getDomainAsMap().get(clusterConfig.getFaultZoneType()));
+        }
+        return instanceConfigMap;
+      }
+
+      @Override
+      public ClusterConfig updateClusterConfig(ClusterConfig clusterConfig) {
+        Map<ClusterConfig.GlobalRebalancePreferenceKey, Integer> preference = new HashMap<>();
+        preference.put(ClusterConfig.GlobalRebalancePreferenceKey.EVENNESS, 1);
+        preference.put(ClusterConfig.GlobalRebalancePreferenceKey.LESS_MOVEMENT, 1);
+        clusterConfig.setGlobalRebalancePreference(preference);
+        clusterConfig.setPersistIntermediateAssignment(false);
+        clusterConfig.setPersistBestPossibleAssignment(true);
+        clusterConfig.setInstanceCapacityKeys(Collections.singletonList("Partition"));
+        clusterConfig.setDefaultInstanceCapacityMap(DEFAULT_CAPACITY);
+        clusterConfig.getRecord().setListField(
+            ClusterConfig.ClusterConfigProperty.STATE_TRANSITION_THROTTLE_CONFIGS.name(),
+            Collections.emptyList());
+        return clusterConfig;
+      }
+    };
+  }
+
+  public static void main(String[] args) throws Exception {
+    String srcZkServers = "zk-ei4-ring.int.linkedin.com:3764";
+    String clusterName = "espresso-mt-ld3";
+    if (args.length >= 2) {
+      srcZkServers = args[0];
+      clusterName = args[1];
+    }
+    CloneClusterSimulator simulator = new CloneClusterSimulator(srcZkServers, 5, true);
+
+    Set<String> faultZones = new HashSet<>();
+    String faultZoneType;
+    String topology;
+
+    HelixZkClient.ZkClientConfig clientConfig = new HelixZkClient.ZkClientConfig();
+    clientConfig.setZkSerializer(new ZNRecordSerializer()).setConnectInitTimeout(3 * 1000);
+    HelixZkClient srcClient = SharedZkClientFactory.getInstance()
+        .buildZkClient(new HelixZkClient.ZkConnectionConfig(srcZkServers), clientConfig);
+    try {
+      ConfigAccessor configAccessor = new ConfigAccessor(srcClient);
+      ClusterConfig clusterConfig = configAccessor.getClusterConfig(clusterName);
+      faultZoneType = clusterConfig.getFaultZoneType();
+      topology = clusterConfig.getTopology();
+      ZKHelixAdmin admin = new ZKHelixAdmin(srcClient);
+      for (String instance : admin.getInstancesInCluster(clusterName)) {
+        String faultZone =
+            admin.getInstanceConfig(clusterName, instance).getDomainAsMap().get(faultZoneType);
+        faultZones.add(faultZone);
+      }
+    } finally {
+      srcClient.close();
+    }
+
+    List<Operation> operations = new ArrayList<>();
+    operations.addAll(ExampleScripts.migrateToWagedRebalancer());
+    operations.addAll(ExampleScripts
+        .expandFaultZones(2, simulator.DEFAULT_CAPACITY, faultZones, topology, faultZoneType,
+            simulator));
+    operations.addAll(ExampleScripts.shrinkFaultZones(2, faultZones, simulator));
+
+    simulator.simulate(ZK_ADDRESS, clusterName, simulator.generateOverwrites(), operations, false);
+    System.exit(0);
+  }
+}
---
+++ helix-core/src/main/java/org/apache/helix/examples/rebalancer/simulator/examples/ExampleScripts.java
@@ -0,0 +1,192 @@
+package org.apache.helix.examples.rebalancer.simulator.examples;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.apache.helix.HelixAdmin;
+import org.apache.helix.HelixException;
+import org.apache.helix.controller.rebalancer.waged.WagedRebalancer;
+import org.apache.helix.examples.rebalancer.simulator.AbstractSimulator;
+import org.apache.helix.examples.rebalancer.simulator.operations.AddNode;
+import org.apache.helix.examples.rebalancer.simulator.operations.AddResource;
+import org.apache.helix.examples.rebalancer.simulator.operations.EnableMaintenanceMode;
+import org.apache.helix.examples.rebalancer.simulator.operations.EnableNode;
+import org.apache.helix.examples.rebalancer.simulator.operations.ModifyResource;
+import org.apache.helix.examples.rebalancer.simulator.operations.Operation;
+import org.apache.helix.examples.rebalancer.simulator.operations.RebootNode;
+import org.apache.helix.examples.rebalancer.simulator.operations.RemoveNode;
+import org.apache.helix.examples.rebalancer.simulator.operations.RemoveResource;
+import org.apache.helix.model.BuiltInStateModelDefinitions;
+import org.apache.helix.model.IdealState;
+import org.apache.helix.model.InstanceConfig;
+import org.apache.helix.zookeeper.api.client.HelixZkClient;
+
+public class ExampleScripts {
+  public static List<Operation> showCaseOperations(int partitionCount, int replicaCount,
+      Map<String, Integer> defaultUsage, String rebalanaceClass, String rebalanceStrategy,
+      AbstractSimulator simulator) {
+    String targetResourceName = "newResource";
+    List<Operation> operations = new ArrayList<>();
+    operations.add(
+        new AddResource(targetResourceName, BuiltInStateModelDefinitions.OnlineOffline.name(),
+            defaultUsage, partitionCount, replicaCount, replicaCount, rebalanaceClass,
+            rebalanceStrategy));
+    String targetNode = "newInstance0_12000";
+    operations.add(new EnableNode(targetNode, false));
+    operations.add(new EnableNode(targetNode, true));
+
+    IdealState newIs = new IdealState(targetResourceName);
+    newIs.setReplicas("1");
+    operations.add(new ModifyResource(null, newIs));
+
+    operations.add(new RebootNode(targetNode, simulator));
+    operations.add(new RemoveNode(targetNode, simulator));
+    operations.add(new RemoveResource(targetResourceName));
+
+    return operations;
+  }
+
+  public static List<Operation> rollingUpgrade(Set<String> faultZones,
+      AbstractSimulator simulator) {
+    List<Operation> operations = new ArrayList<>();
+    for (String zone : faultZones) {
+      // disable
+      operations.add(new Operation() {
+        @Override
+        public boolean execute(HelixAdmin admin, HelixZkClient zkClient, String clusterName) {
+          List<String> nodes = simulator.getNodeToZoneMap().entrySet().stream()
+              .filter(entry -> entry.getValue().equals(zone)).map(entry -> entry.getKey())
+              .collect(Collectors.toList());
+          for (String node : nodes) {
+            InstanceConfig instanceConfig = admin.getInstanceConfig(clusterName, node);
+            instanceConfig.setInstanceEnabled(false);
+            admin.setInstanceConfig(clusterName, node, instanceConfig);
+          }
+          return true;
+        }
+
+        @Override
+        public String getDescription() {
+          return "Disable all nodes in zone: " + zone;
+        }
+      });
+      // reset and re-enable
+      operations.add(new Operation() {
+        @Override
+        public boolean execute(HelixAdmin admin, HelixZkClient zkClient, String clusterName) {
+          List<String> nodes = simulator.getNodeToZoneMap().entrySet().stream()
+              .filter(entry -> entry.getValue().equals(zone)).map(entry -> entry.getKey())
+              .collect(Collectors.toList());
+          for (String node : nodes) {
+            try {
+              simulator.resetProcess(node);
+            } catch (Exception e) {
+              return false;
+            }
+            InstanceConfig instanceConfig = admin.getInstanceConfig(clusterName, node);
+            instanceConfig.setInstanceEnabled(true);
+            admin.setInstanceConfig(clusterName, node, instanceConfig);
+          }
+          return true;
+        }
+
+        @Override
+        public String getDescription() {
+          return "Reset and then re-enable all nodes in zone: " + zone;
+        }
+      });
+    }
+    return operations;
+  }
+
+  public static List<Operation> migrateToWagedRebalancer() {
+    List<Operation> operations = new ArrayList<>();
+    operations.add(new EnableMaintenanceMode(true));
+    operations.add(new Operation() {
+      @Override
+      public boolean execute(HelixAdmin admin, HelixZkClient zkClient, String clusterName) {
+        for (String resource : admin.getResourcesInCluster(clusterName)) {
+          IdealState is = admin.getResourceIdealState(clusterName, resource);
+          is.setRebalancerClassName(WagedRebalancer.class.getName());
+          admin.setResourceIdealState(clusterName, resource, is);
+        }
+        return true;
+      }
+
+      @Override
+      public String getDescription() {
+        return "Use WAGED rebalancer for all resources.";
+      }
+    });
+    operations.add(new EnableMaintenanceMode(false));
+    return operations;
+  }
+
+  public static List<Operation> expandFaultZones(int newNodeCountForEachZone,
+      Map<String, Integer> capacityMap, Set<String> faultZones, String topology,
+      String faultZoneType, AbstractSimulator simulator) {
+    List<Operation> operations = new ArrayList<>();
+    operations.add(new EnableMaintenanceMode(true));
+    for (String faultZone : faultZones) {
+      for (int i = 0; i < newNodeCountForEachZone; i++) {
+        String nodeName = "expansionNodes_" + i + "_" + faultZone;
+        StringBuilder domainStrBuilder = new StringBuilder();
+        String[] keys = topology.split("/");
+        int j = 0;
+        for (; j < keys.length - 1; j++) {
+          String key = keys[j];
+          if (!key.isEmpty()) {
+            if (key.equals(faultZoneType)) {
+              domainStrBuilder.append(key + "=" + faultZone + ",");
+            } else {
+              domainStrBuilder.append(key + "=DEFAULT,");
+            }
+          }
+        }
+        domainStrBuilder.append(keys[j] + "=" + nodeName);
+        operations.add(new AddNode(nodeName, i, capacityMap, faultZone, domainStrBuilder.toString(),
+            simulator));
+      }
+    }
+    operations.add(new EnableMaintenanceMode(false));
+    return operations;
+  }
+
+  public static List<Operation> shrinkFaultZones(int removingNodeForEachZone,
+      Set<String> faultZones, AbstractSimulator simulator) {
+    List<Operation> operations = new ArrayList<>();
+    operations.add(new EnableMaintenanceMode(true));
+    for (String faultZone : faultZones) {
+      operations.add(new Operation() {
+        @Override
+        public boolean execute(HelixAdmin admin, HelixZkClient zkClient, String clusterName) {
+          List<String> nodes = simulator.getNodeToZoneMap().entrySet().stream()
+              .filter(entry -> entry.getValue().equals(faultZone)).map(entry -> entry.getKey())
+              .collect(Collectors.toList());
+          for (int i = 0; i < removingNodeForEachZone; i++) {
+            String instanceName = nodes.get(i);
+            simulator.stopProcess(instanceName);
+            admin.dropInstance(clusterName, admin.getInstanceConfig(clusterName, instanceName));
+            try {
+              admin.getInstanceConfig(clusterName, instanceName);
+              return false;
+            } catch (HelixException ex) {
+              simulator.getNodeToZoneMap().remove(instanceName);
+            }
+          }
+          return true;
+        }
+
+        @Override
+        public String getDescription() {
+          return String
+              .format("Remove %d nodes from fault zone %s", removingNodeForEachZone, faultZone);
+        }
+      });
+    }
+    operations.add(new EnableMaintenanceMode(false));
+    return operations;
+  }
+}
